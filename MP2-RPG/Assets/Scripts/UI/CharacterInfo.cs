ec3a50942f644439d87c8b55ee0e0a629fa41ad4
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2025-02-27 18:34:12 -0500

    began work on the dialog manager and collision

--- MP2-RPG/Assets/Scripts/UI/CharacterInfo.cs
+++ MP2-RPG/Assets/Scripts/UI/CharacterInfo.cs
@@ -5,5 +5,5 @@ public class CharacterInfo : MonoBehaviour
 {
     [SerializeField] private TextBox m_textBox;
 
-    public TextBox TextBox { get => m_textBox; }
+    public TextBox DialogTextBox { get => m_textBox; }
 }
--- MP2-RPG/Assets/Scripts/UI/DialogManager.cs
+++ MP2-RPG/Assets/Scripts/UI/DialogManager.cs
@@ -5,4 +5,22 @@ public class DialogManager : MonoBehaviour
     //search the area around thep layer, if he hits an object that is a char info, put it on screen and wait
     //2 options for the player, using standard buttons see through (ttogle on and off when chatting, maybe use a nuified script to control buttons and reuse movement?) 
     //drive dialog through the buttons
+    public Transform textbox;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check for the character script, if found, load the camera and change the input managemr
+    }
+    private void RetrieveTextBoxInfoFromCharacter(CharacterInfo info)
+    {
+
+    }
+    void SwapModeOfControl()
+    {
+
+    }
+    private void DisplayTextBoxWithInfo(CharacterInfo info)
+    {
+        textbox.gameObject.SetActive(true);
+        info.DialogTextBox.PopulateTextBoxPrefab(textbox.gameObject);
+    }
 }
